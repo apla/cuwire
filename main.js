92d1a0f042106bc88f28a0295e97a47c52f8bf4f
apla <dot.and.thing@gmail.com>
2014-09-27 18:39:15 +0400

    now with boards enumeration

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ define(function (require, exports, module) {
 	ArduinoExt.prototype.enumerateSerialPorts = function () {
 		// TODO: show spinner indicator
 
-		this.domain.exec("enumerateSerialPorts", false)
+		this.domain.exec("enumerateSerialPorts")
 		.done(function (ports) {
 			// TODO: get last used port from preference manager
 			// TODO: show warning indicator
@@ -46,7 +46,53 @@ define(function (require, exports, module) {
 			//		$('<td />').text(err.filename).appendTo(tr);
 		}).fail(function (err) {
 			// TODO: show error indicator
-			console.error("[brackets-arduino-node] failed to run arduino.enumerateSerialPorts", err);
+			console.error("[brackets-arduino-node] failed to run arduino.enumerateSerialPorts, error:", err);
+		});
+
+	}
+
+	ArduinoExt.prototype.getBoardMeta = function () {
+		// TODO: show spinner indicator
+
+		this.domain.exec("getBoardsMeta", ["/Users/apla/Documents/Arduino"])
+		.done(function (platforms) {
+			console.log("[brackets-arduino-node] Available boards:");
+
+			$('#arduino-panel ul.arduino-board li').remove();
+			// tr = $('<tr />').appendTo('#arduino-panel tbody');
+			var arduinoBoardDD = $('#arduino-panel ul.arduino-board');
+
+			Object.keys (platforms).forEach (function (platform) {
+				console.log (platform);
+				$('<li class="dropdown-header">'
+				  + platforms[platform].platform.name + " "
+				  + platforms[platform].platform.version
+				  + "</li>").appendTo(arduinoBoardDD);
+
+				var boards = platforms[platform].boards;
+				Object.keys (boards).map (function (boardId) {
+					var boardMeta = boards[boardId];
+					var boardDesc = boardMeta.name + ' (' + boardId
+					if ("menu" in boardMeta) {
+						boardDesc += ', variants: ';
+						var variants = [];
+						for (var cpuVariant in boardMeta.menu.cpu) {
+							variants.push (boardMeta.menu.cpu[cpuVariant].cpu_variant_name);
+						}
+						boardDesc += variants.join (",");
+
+					}
+					boardDesc += ')';
+					console.log (boardDesc);
+
+					$('<li><a href="#">'+boardMeta.name+"</a></li>").appendTo(arduinoBoardDD);
+
+				});
+			});
+
+		}).fail(function (err) {
+			// TODO: show error indicator
+			console.error("[brackets-arduino-node] failed to run arduino.getBoardMeta, error:", err);
 		});
 
 	}
@@ -61,6 +107,7 @@ define(function (require, exports, module) {
 		this.panel = PanelManager.createBottomPanel("Arduino", $(require('text!bottom-panel.html')));
 
 		this.enumerateSerialPorts ();
+		this.getBoardMeta ();
 
 		myIcon.on ("click", this.panel.show.bind (this.panel));
 		$('#arduino-panel .close').on('click', this.panel.hide.bind (this.panel));
--- node/ArduinoDomain.js
+++ node/ArduinoDomain.js
@@ -6,7 +6,167 @@ maxerr: 50, node: true */
 	"use strict";
 
 	var os = require("os");
+	var fs   = require('fs');
+	var path = require ('path');
 
+
+	function parseArduinoConf (cb, section, err, data) {
+		if (err) {
+			cb (err);
+			return;
+		}
+
+		var boards = {};
+
+		data.toString().split('\n').forEach(function(line){
+			if(line.indexOf("#") == 0) return;
+			if(line.length == 0) return;
+			// console.log (line);
+			var parts = line.split('=');
+			var ref = parts[0];
+			if (ref === "menu.cpu") return;
+			var value = parts[1];
+			var refs = ref.split('.');
+
+			var root = boards;
+			if (refs.length === 4 && refs[1] === "menu" && refs[2] === "cpu")
+				refs.push ("cpu_variant_name");
+			for(var i=0; i<refs.length; i++) {
+				var sec = refs[i];
+				if(!root[sec]) {
+					root[sec] = {};
+				}
+				if(i == refs.length-1) {
+					root[sec] = value;
+				}
+				root = root[sec];
+			}
+		});
+		cb (null, section, boards);
+
+	}
+
+	function appendStandardLocations (locations) {
+		// 1.0 /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/boards.txt
+		// 1.5 /Applications/Arduino.app/Contents/Java/hardware/arduino/avr/boards.txt
+
+		// default application folders:
+		locations.push ("/Applications/Arduino.app");
+		locations.push ("C:/Program Files/Arduino");
+
+		// default user folders:
+		function getUserHome() {
+			return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+		}
+		// TODO: read preference file ~/Library/Arduino15/preferences.txt
+		locations.push (path.join (getUserHome(), "Documents/Arduino"));
+
+		// TODO: author's module location - use preferences for this
+		locations.push ("/Applications/devel/Arduino.app");
+
+	}
+
+	function getBoardsMeta (locations) {
+
+		var cb = arguments[arguments.length-1];
+
+		if (locations.constructor !== Array) {
+			if (locations.constructor === String) {
+				locations = [locations];
+			} else {
+				console.log (arguments);
+				cb ("first argument must be a path string or an array of paths");
+			}
+		}
+
+		appendStandardLocations (locations);
+
+		var hwFolders = [];
+		[
+			"Contents/Java/hardware",
+			"hardware"
+		].forEach (function (append) {
+			locations.forEach (function (location) {
+				if (!location || location.constructor !== String) {
+					cb ("every location must be string");
+				}
+				hwFolders.push (path.join (location, append));
+			});
+		});
+
+		console.log (hwFolders);
+
+		var data = {};
+		var remains = 0;
+		var processed = false;
+		// WTF spaghetti
+		hwFolders.forEach (function (hwFolder, hwFolderIdx) {
+			// ok, got hardware folder. now we must iterate over vendor folders
+			// and the by devices folders
+			fs.readdir (hwFolder, function (err, vendorDirs) {
+				if (err) {
+					if (hwFolderIdx === hwFolders.length - 1)
+						processed = true;
+					return;
+				}
+				vendorDirs.forEach (function (vendorDir, vendorIdx) {
+					fs.readdir (path.join (hwFolder, vendorDir), function (err, deviceDirs) {
+						if (err) {
+							if (vendorIdx === vendorDirs.length - 1)
+								processed = true;
+							return;
+						}
+						deviceDirs.forEach (function (deviceDir, deviceIdx) {
+							var readCb = function (err, type, fileData) {
+								remains --;
+								if (err) {
+									return;
+								}
+
+								if (!data[vendorDir+": "+deviceDir])
+									data[vendorDir+": "+deviceDir] = {};
+								data[vendorDir+": "+deviceDir][type] = fileData;
+								// TODO: probably race condition: we can achieve no more remains
+								// but still unread folders
+								// console.log ("processed:", processed, "remains:", remains);
+								if (!processed)
+									return;
+								if (remains)
+									return;
+								arduinoBoardsDone (cb, data);
+							};
+							var deviceBoardsFile   = path.join (hwFolder, vendorDir, deviceDir, 'boards.txt');
+							var devicePlatformFile = path.join (hwFolder, vendorDir, deviceDir, 'platform.txt');
+							remains += 2;
+
+							// console.log ("reading", deviceBoardsFile);
+							fs.readFile (deviceBoardsFile, parseArduinoConf.bind (this, readCb, 'boards'));
+							// console.log ("reading", devicePlatformFile);
+							fs.readFile (devicePlatformFile, parseArduinoConf.bind (this, readCb, 'platform'));
+
+							if (
+								(hwFolderIdx === hwFolders.length - 1) &&
+								(vendorIdx === vendorDirs.length - 1) &&
+								(deviceIdx === deviceDirs.length - 1)
+							) {
+								processed = true;
+							}
+						});
+					});
+				});
+			});
+		});
+	}
+
+	function arduinoBoardsDone (cb, boards) {
+		fs.writeFile (
+			path.join (__dirname, "../arduino.json"),
+			JSON.stringify (boards, null, '\t'),
+			function (err) {
+				if (err) cb (err);
+				cb (null, boards);
+			});
+	}
 
 	/**
 	 * function to enumerate serial ports
@@ -16,12 +176,11 @@ maxerr: 50, node: true */
 //		console.log (os);
 
 		var cb = arguments[arguments.length-1];
-//		console.log (cb, arguments);
 
 		var serialport;
 		try {
 			// https://github.com/voodootikigod/node-serialport
-			// HOWTO built THAT on mac:
+			// HOWTO built THAT on mac (got idea from https://github.com/jasonsanjose/brackets-sass/tree/master/node):
 			// 1) cd <extension-folder>/node; npm install node-gyp node-pre-gyp serialport
 			// 2) cd node_modules/serialport
 			// 3) /Applications/devel/Brackets.app/Contents/MacOS/Brackets-node ../../node_modules/node-pre-gyp/bin/node-pre-gyp --arch=ia32 rebuild
@@ -67,6 +226,21 @@ maxerr: 50, node: true */
 			[{name: "ports", // return values
 			  type: "array",
 			  description: "serial port path names"}]
+		);
+		domainManager.registerCommand(
+			"arduino",       // domain name
+			"getBoardsMeta",    // command name
+			getBoardsMeta,   // command handler function
+			true,          // this command is asynchronous in Node
+			"get arduino boards metadata",
+			[{
+				name: "dirs",
+				type: "array",
+				description: "directory list to search within"
+			}],
+			[{name: "boards", // return values
+			  type: "object",
+			  description: "board data"}]
 		);
 	}
 
