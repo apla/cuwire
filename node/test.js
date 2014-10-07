28f289e2141e92fb9f6ca8df9934d46f29b50732
apla <dot.and.thing@gmail.com>
2014-10-07 20:16:25 +0400

    nodejs test

---
+++ node/test.js
@@ -0,0 +1,17 @@
+var ard = require ('./arduino');
+
+var arduino = new ard (["/Applications/devel/Arduino.app"]);
+
+arduino.on ('done', function () {
+	var platformName = "arduino/avr";
+
+	var boardId = "pro";
+
+	var cpuId = "16MHzatmega328";
+
+	arduino.compile (
+		"/Users/apla/work/com.domtale/arduino/Sensor", // sketch folder
+		"/Users/apla/Library/Application Support/Brackets/extensions/user/brackets-arduino/build", // build folder
+		platformName, boardId, cpuId
+	);
+});
