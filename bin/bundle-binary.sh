dbd589a8030a5480602ddcf065ae31091c0a69ac
apla <dot.and.thing@gmail.com>
2015-01-29 02:17:51 +0300

    removing brackets

--- bin/bundle-binary.sh
+++
@@ -1,4 +0,0 @@
-cd node
-npm install node-gyp node-pre-gyp serialport
-cd node_modules/serialport
-/Applications/devel/Brackets.app/Contents/MacOS/Brackets-node ../../node_modules/node-pre-gyp/bin/node-pre-gyp --arch=ia32 rebuild
